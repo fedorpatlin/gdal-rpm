6d2bd33e175396bca5e342837d9377a85da0e2eb
fedor patlin <patlin.f@sovzond.center>
2015-12-29 10:37:33 +0500

    commit fails if no tty

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 this=$(readlink -f $0)
 workdir=$(dirname "$this")
-echo WORKDIR is $workdir
 docker build --force-rm -t gdal-rpm .
 docker run -it --rm -v $workdir/src:/root/src -v $workdir/dest:/root/dest gdal-rpm
 docker rmi gdal-rpm
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 sourcedir=rpmbuild/SOURCES
 specdir=rpmbuild/SPECS
-cp -v src/gdal-1.9.0-java.patch $sourcedir
-cp -v src/gdal-cleaner.sh $sourcedir
-cp -v src/gdal-g2clib.patch $sourcedir
-cp -v src/gdal-jni.patch $sourcedir
-cp -v src/gdal.pom $sourcedir
-cp -v src/PROVENANCE.TXT-fedora $sourcedir
+cp  src/gdal-1.9.0-java.patch $sourcedir
+cp  src/gdal-cleaner.sh $sourcedir
+cp  src/gdal-g2clib.patch $sourcedir
+cp  src/gdal-jni.patch $sourcedir
+cp  src/gdal.pom $sourcedir
+cp  src/PROVENANCE.TXT-fedora $sourcedir
 
-cp -v src/gdal.spec $specdir
+cp  src/gdal.spec $specdir
 
 rpmbuild -ba $specdir/gdal.spec
 
-cp -rfvu rpmbuild/RPMS rpmbuild/SRPMS dest/
+cp -rfu rpmbuild/RPMS rpmbuild/SRPMS dest/
