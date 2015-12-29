6539a336491c71d3fddb28d7bd8947dcd9de61d6
fedor patlin <patlin.f@sovzond.center>
2015-12-29 11:25:11 +0500

    docker command line updated

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 this=$(readlink -f $0)
 workdir=$(dirname "$this")
 docker build --force-rm -t gdal-rpm .
-docker run -it --rm -v $workdir/src:/root/src -v $workdir/dest:/root/dest gdal-rpm
+docker run --rm -v $workdir/src:/root/src -v $workdir/dest:/root/dest gdal-rpm
 docker rmi gdal-rpm
